5c981d5f9d9d0494f2d059ab539289dcc04d880a
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-27 20:34:36 +0530

    changes

--- userdata.sh
+++ userdata.sh
@@ -2,5 +2,5 @@
 
 set-hostname ${component}
 yum install ansible python3.12-pip.noarch -y &>>/opt/userdata.log
-pip3.12 install botocore boto3
+sudo pip3.12 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/Revanthsatyam/roboshop-ansible-d76.git -e component=${component} -e env=${env} main.yml &>>/opt/userdata.log
