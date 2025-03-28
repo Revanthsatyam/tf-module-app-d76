6c59c541d9928487f5f5211d1d058c6905cb8e39
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-28 13:25:33 +0530

    changes

--- main.tf
+++ main.tf
@@ -134,6 +134,16 @@ resource "aws_lb_target_group" "private" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
+
+  health_check {
+    protocol            = "HTTP"
+    path                = "/health"
+    port                = 80
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
 }
 
 resource "aws_lb_listener_rule" "private" {
