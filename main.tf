c40488c639bd9fea0772e0268328dcc88af1678b
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-28 12:56:50 +0530

    changes

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "aws_lb_target_group" "private" {
 }
 
 resource "aws_lb_listener_rule" "private" {
-  listener_arn = "arn:aws:elasticloadbalancing:us-east-1:058264090525:listener/app/private-alb/facda60c735a2144/4e503234ef9a0c78"
+  listener_arn = "arn:aws:elasticloadbalancing:us-east-1:058264090525:listener/app/private-alb/f748d9f123558797/5f05a1e79d775003"
   priority     = var.priority
 
   action {
