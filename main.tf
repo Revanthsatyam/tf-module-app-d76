3df0e2e219bef22de0def33260970939a7703c4d
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-28 16:24:29 +0530

    changes

--- main.tf
+++ main.tf
@@ -163,29 +163,29 @@ resource "aws_lb_listener_rule" "private" {
   }
 }
 
-# resource "aws_lb_target_group" "public" {
-#   count                = var.app_name == "frontend" ? 1 : 0
-#   name                 = "${local.name_prefix}-alb-public-tg"
-#   port                 = var.sg_port
-#   protocol             = "HTTP"
-#   vpc_id               = var.vpc_id
-#   deregistration_delay = 15
-#
-#   health_check {
-#     enabled             = true
-#     healthy_threshold   = 2
-#     interval            = 5
-#     matcher             = "404"
-#     path                = "/"
-#     port                = var.sg_port
-#     timeout             = 2
-#     unhealthy_threshold = 2
-#   }
-# }
-#
-# resource "aws_lb_target_group_attachment" "test" {
-#   count            = var.app_name == "frontend" ? 1 : 0
-#   target_group_arn = aws_lb_target_group.public[0].arn
-#   target_id        = aws_instance.test.id
-#   port             = var.sg_port
-# }
+resource "aws_lb_target_group" "public" {
+  count                = var.app_name == "frontend" ? 1 : 0
+  name                 = "${local.name_prefix}-alb-public-tg"
+  port                 = var.sg_port
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  deregistration_delay = 15
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    matcher             = "404"
+    path                = "/"
+    port                = var.sg_port
+    timeout             = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  count            = var.app_name == "frontend" ? 1 : 0
+  target_group_arn = aws_lb_target_group.public[0].arn
+  target_id        = data.dns_a_record_set.private_alb_name.addrs
+  port             = var.sg_port
+}
