512c769553a6d6d2b1e12bc411e4248f8d534c3e
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-09 17:50:41 +0530

    changes

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   name_prefix      = "${var.app_name}-${var.env}"
   tags             = merge(var.tags, { Name = "tf-module-app" }, { env = var.env })
   parameters       = var.parameters
-  policy_resources = [ "arn:aws:ssm:us-east-1:058264090525:parameter/*", var.kms_key ]
+  policy_resources = [ for i in local.parameters : "arn:aws:ssm:us-east-1:058264090525:parameter/${i}.*" ]
 }
